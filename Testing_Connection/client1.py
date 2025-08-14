3725ff5278b1ffd20825bc00d84d85e30c41667e
Malikwilliamz <malikwilliams10750@gmail.com>
2025-08-13 20:13:20 -0600

    first commit

---
+++ Testing_Connection/client1.py
@@ -0,0 +1,10 @@
+import socket
+
+HOST ="192.168.68.70"
+PORT = 65432
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+client.connect((HOST, PORT))
+
+client.send('Hello From Client'.encode())
+print(client.recv(1024).decode())

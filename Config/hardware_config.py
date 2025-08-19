686bcdf2548b448b2fb1777f509061f7d648b3ff
Malikwilliamz <malikwilliams10750@gmail.com>
2025-08-18 20:08:55 -0600

    Changes to hardware_config.py

--- Config/hardware_config.py
+++ Config/hardware_config.py
@@ -7,6 +7,7 @@ import piplates.MOTORplate as MOTOR
 # Motor plate configuration
 MOTOR.dcCONFIG(0, 1, 'cw', 50.0, 2.5)
 
+
 def run_motor1():
     MOTOR.dcSTART(0, 1)
     time.sleep(5.0)
@@ -15,3 +16,20 @@ def run_motor1():
     MOTOR.dcSTOP(0, 1)
 
     print("Motor 1 run complete.")
+
+def move_forward():
+    MOTOR.dcCONFIG(0, 1, 'ccw', 50.0, 0)
+    MOTOR.dcCONFIG(0, 2, 'ccw', 50.0, 0)
+    MOTOR.dcCONFIG(0, 3, 'cw', 50.0, 0)
+    MOTOR.dcCONFIG(0, 4, 'cw', 50.0, 0)
+
+    MOTOR.dcSTART(0, 1)
+    MOTOR.dcSTART(0, 2)
+    MOTOR.dcSTART(0, 3)
+    MOTOR.dcSTART(0, 4)
+    
+def stop_all():
+    MOTOR.dcSTOP(0, 1)
+    MOTOR.dcSTOP(0, 2)
+    MOTOR.dcSTOP(0, 3)
+    MOTOR.dcSTOP(0, 4)

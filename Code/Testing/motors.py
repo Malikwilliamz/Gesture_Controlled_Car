41174e953746eb693bbef69dfd8899a80aba879b
Malikwilliamz <malikwilliams10750@gmaill.com>
2025-08-20 00:44:54 -0400

    Configured Left-turn

--- Code/Testing/motors.py
+++ Code/Testing/motors.py
@@ -5,7 +5,7 @@ This File tests motor functionalities based o.
 import sys 
 import time
 
-sys.path.append('/home/malo/Documents/Gesture_Controlled_Car/Config')
+sys.path.append('/home/malik/Documents/Gesture_Controlled_Car/Gesture_Controlled_Car/Config')
 
 from hardware_config import move_forward, move_backward, turn_left, stop_all
 
@@ -18,24 +18,22 @@ def test_all_motors():
 
 def test_left_turn():
     turn_left()
-    time.sleep(5)
     stop_all()
 
 def main():
-    while True:
-        select_test = input("Select test to run (1: All Motors, 2: Turn Left 9: Exit): ")
-        print(f"Selected test: {select_test}")
-        if select_test == "1":
-            test_all_motors()
+    while True:    
+       select_test = input("Select test to run (1: All Motors, 2: Turn Left 9: Exit): ")
+       print(f"Selected test: {select_test}")
+       if select_test == "1":
+          test_all_motors()
 
-        elif select_test == "2":
-            test_left_turn()
-
-        elif select_test == "9":
-            break
-
-        else:
-            print("Invalid selection.")
+       elif select_test == "2":
+          test_left_turn()
+       
+       elif select_test == "9":
+          break
+       else:
+          print("Invalid selection.")
 
 
 if __name__ == "__main__":
